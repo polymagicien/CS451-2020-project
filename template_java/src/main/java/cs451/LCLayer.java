d44dd7c77c6f5591653c580798478effacdf21b1
Sam <samuel.thirion1@gmail.com>
2020-11-28 09:26:28 +0100

    Update LCLayer

--- template_java/src/main/java/cs451/LCLayer.java
+++ template_java/src/main/java/cs451/LCLayer.java
@@ -7,14 +7,20 @@ import java.util.List;
 
 public class LCLayer implements Layer {
 
-    private Map<Host, Integer> VC;
+	// private Map<Host, Integer> vc;
+	private Host me;
+    private Layer urbLayer;
+	private Layer upperLayer;
+	
+    private int[] vc;
+    private Map<Host, List<Host>> dependency;
 
     LCLayer(List<Host> hosts, Host me) {
-        VC = new HashMap<>();
+		this.me = me;
+		this.urbLayer = new UrbLayer(hosts, me);
+		this.urbLayer.deliverTo(this);
 
-        for(Host host : hosts) {
-            VC.put(host, 0);
-        }
+		this.vc = new int[hosts.size() + 1];
 
 
     }
@@ -30,18 +36,51 @@ public class LCLayer implements Layer {
 	}
 
 	public void deliverTo(Layer layer) {
-		// TODO Auto-generated method stub
-		
+        this.upperLayer = layer;
 	}
 
+	public void deliver(Host host, String message) {
+        if (upperLayer != null) {
+            upperLayer.receive(host, message);
+        } else {
+            System.out.println("FIFO : " + host + " - " + message);
+        }
+    }
+
 	public void handleCrash(Host crashedHost) {
-		// TODO Auto-generated method stub
-		
+				
 	}
 
 	public String waitFinishBroadcasting(boolean retString) {
-		// TODO Auto-generated method stub
 		return null;
 	}
+
+	private static int[] parseVC(String strVC) {
+        String[] v = strVC.split(";");
+
+        int[] vc = new int[v.length];
+        for (int i = 0; i < v.length; i++) {
+			int hostId = Integer.valueOf(v[i].split(":")[0]);
+			int require = Integer.valueOf(v[i].split(":")[1]);
+			vc[hostId] = require;
+		}
+        return vc;
+    }
+
+	private String getStringDependency(List<Host> dependencies) {
+		String s = "";
+		if (dependencies.size() == 0)
+			return s;
+
+        for (int i = 0; i < dependencies.size(); i++) {
+            int hId = dependencies.get(i).getId();
+            s += hId;
+            s += ":";
+            s += vc[hId]; 
+            s += ";";
+		}
+		s = s.substring(0, s.length() - 1);
+        return s;
+    }
     
 }
